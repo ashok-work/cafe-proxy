a3b7d2b20af65b69df19922b37d0c80e05502167
ashok <inashok123@gmail.com>
2022-04-01 12:03:16 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {createProxyMiddleware} = require("http-proxy-middleware");
 
 // Create Express Server
 const app = express();
+const cors = require('cors');
 
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+app.use(cors());
 // Configuration
 const API_SERVICE_URL = 'https://fzstaging.fc.qwikcilver.com/api/customer/';
 
@@ -13,18 +19,24 @@ app.use(morgan("dev"));
 
 // Proxy Logic : Proxy endpoints
 app.use("/",
-	createProxyMiddleware({
-		target: API_SERVICE_URL,
-		changeOrigin: true,
-		logLevel: 'debug',
-	})
+    createProxyMiddleware({
+        target: API_SERVICE_URL,
+        changeOrigin: true,
+        logLevel: 'debug',
+    })
 );
 
-app.get("*", function (req, res) {
-	res.json({status: true});
+// Listen both http & https ports
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/proxy.ashok.work/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/proxy.ashok.work/fullchain.pem'),
+}, app);
+
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
 });
 
-// Starting our Proxy server
-app.listen(80, () => {
-	console.log(`Starting Proxy at ${process.env.PORT || 5000}`);
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
 });
